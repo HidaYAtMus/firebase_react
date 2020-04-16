8848a9467870f2da689048edebfbfce4537a8352
debyprast <rio.mus48@gmail.com>
2020-04-16 13:31:56 +0700

    menambahkan source code

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import routes from "./routes.js";
 import Header from "./Header";
 import "./styles.css";
-import * as firebase from "firebase";
-import firebaseConfig from "./firebase.config";
-
-firebase.initializeApp(firebaseConfig);
 
 export const AuthContext = React.createContext(null);
 
